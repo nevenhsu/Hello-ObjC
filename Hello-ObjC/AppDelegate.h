a4a5f45112bfd10760b7d1aeb83a69be82505039
許乃文 <space.man0318@gmail.com>
2017-10-28 23:26:56 +0800

    Initial Commit

---
+++ Hello-ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello-ObjC
+//
+//  Created by Neven on 28/10/2017.
+//  Copyright © 2017 Neven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello-ObjC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Hello-ObjC
+//
+//  Created by Neven on 28/10/2017.
+//  Copyright © 2017 Neven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
